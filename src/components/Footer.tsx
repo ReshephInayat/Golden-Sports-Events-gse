a79a1d6f6063386e33da1ede4c4344869aad8534
Resheph Inayat <rasafinayat@gmail.com>
2024-01-27 23:22:06 +0500

    Follow Us

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@
 import {
   EnvelopeIcon,
 } from "@heroicons/react/16/solid";
+import Link from "next/link";
 import React from "react";
+import { BsInstagram } from "react-icons/bs";
 import { FaPhoneVolume } from "react-icons/fa";
 import { TfiLocationPin } from "react-icons/tfi";
 import { Element } from "react-scroll";
@@ -46,11 +48,11 @@ const Footer = () => {
                 Phone
               </h1>
               <p className="text-[17px] w-[100%] text-white opacity-60">
-              <a href="tel:+923007264746" title="Click to Contact Golden Sports Events">
+              <a href="tel:03007264746" title="Click to Contact Golden Sports Events">
                 0300-7264746
               </a>
                 <br />
-                <a href="tel:+923219436596" title="Click to Contact Golden Sports Events">
+                <a href="tel:03219436596" title="Click to Contact Golden Sports Events">
                 0321-9436596
                 </a>
               </p>
@@ -63,7 +65,7 @@ const Footer = () => {
             >
               <EnvelopeIcon className="md:w-[4rem] md:h-[4rem] w-[3rem] h-[3rem] text-black" />
             </div>
-            <a href={`mailto:${recipientEmail}` }title="Click to Email Golden Sports Events" target="_blank">
+            <a href={`mailto:${recipientEmail}` }title="Click to Email Golden Sports Events" target="_blank" >
               
             <div>
               <h1 className="text-[25px] mb-[0.2rem] font-semibold text-[white] ">
@@ -82,9 +84,16 @@ const Footer = () => {
           </div>
         </div>
         <div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between ">
-          <div className="md:text-[18px]text-sm  md:mb-0 text-white opacity-20 ">
+          <div className="text-[18px] mb-[2rem] md:mb-0 text-white opacity-20 ">
             Golden Sports Events | All Rights Reserved
           </div>
+          <div className="flex items-center space-x-10">
+            <p className="text-[18px] text-white opacity-20">Follow Us on → </p>
+            <Link href={'https://www.instagram.com/goldensportseventsgse?igsh=d29xOHR3dTZvZW5z'}>
+            <p><BsInstagram className="text-3xl text-white" title="Instagram" /></p>
+            </Link>
+          </div>
+          <div className="flex items-center "></div>
         </div>
       </div>
     </Element>
