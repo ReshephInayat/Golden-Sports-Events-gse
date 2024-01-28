7cc70fd69c92b6ebbd86a0265ea8dab4dab3f509
Resheph Inayat <rasafinayat@gmail.com>
2024-01-29 00:13:23 +0500

    Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Rajdhani } from "next/font/google";
 import "./globals.css";
 
-const inter = Rajdhani({ weight: "400", subsets: ["latin"] });
+const inter = Rajdhani({ weight: "400", subsets: ["latin"], display: "swap"});
 
 export const metadata: Metadata = {
   title: "Golden Sports Events",
